2843f1caf3a4510476602d9fed2e1326378fb600
jimmy <jimmy@192.168.0.106>
2026-06-18 17:10:33 -0400

    fix: variables CSS de marca definidas globalmente en layout

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Poppins } from 'next/font/google'
 import { MobileTabBar } from '../components/shop/MobileTabBar'
+import { BRAND } from '@/lib/colors'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -22,13 +23,15 @@ export default function RootLayout({
   return (
     <html lang="es" className={poppins.variable}>
       <body
-        className="pb-20 md:pb-0"
+        className="pb-20 md-860:pb-0"
         style={{
           margin: 0,
           padding: 0,
           fontFamily: 'var(--font-poppins), sans-serif',
           background: '#F5F7FA',
-        }}
+          '--brand-red': BRAND.red,
+          '--brand-blue': BRAND.blue,
+        } as React.CSSProperties}
       >
         {children}
         <MobileTabBar />
